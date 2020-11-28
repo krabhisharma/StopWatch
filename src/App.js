a22b4bcd72361fb6ebf26ad3cfc81bd99c8ac4d1
krabhisharma <59818632+krabhisharma@users.noreply.github.com>
2020-11-28 17:36:05 +0530

    added time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Show from './components/show_time';
+import Btn from './components/button';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var newdMs = time.ms, newdS = time.s, newdM = time.m, newdH = time.h;
+
+  const run = () => {
+    if(newdM === 60){
+      newdH++;
+      newdM = 0;
+    }
+    if(newdS === 60){
+      newdM++;
+      newdS = 0;
+    }
+    if(newdMs === 60){
+      newdS++;
+      newdMs = 0;
+    }
+    newdMs++;
+    return setTime({ms:newdMs, s:newdS, m:newdM, h:newdH});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">  
+      <div className="stopwatch">
+        <Show time={time}/>
+        <Btn status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+      </div>
     </div>
   );
 }
